bfd4b5bb9b9d482a1928effe1229ebd13aa0a899
Alexander Forslund <alex@forslund.dev>
2026-07-17 21:21:35 +0200

    feat(engine): Functionally complete registry
    Now includes checking if components exist, as well as grabbing the view
    with a certain set of component types.

--- include/registry.hpp
+++ include/registry.hpp
@@ -2,6 +2,7 @@
 
 #include "engine.hpp"
 #include "sparse.hpp"
+#include <cassert>
 #include <iostream>
 #include <memory>
 #include <queue>
@@ -47,8 +48,61 @@ class Registry {
             component_arrays[type_index]->remove(id);
         } else {
             std::cerr << "[ERROR] Failed to remove component from id: " << id
-                      << ". Compnent does not exist on this id." << std::endl;
+                      << ". Component does not exist on this id." << std::endl;
         }
+    }
+
+    template <typename T> SparseSet<T> *get_component_set() {
+        std::type_index type_index = typeid(T);
+
+        if (component_arrays.find(type_index) != component_arrays.end()) {
+            return static_cast<SparseSet<T> *>(
+                component_arrays[type_index].get());
+        }
+        return nullptr;
+    }
+
+    template <typename T> bool has_component(EntityID id) {
+        std::type_index type_index = typeid(T);
+        if (component_arrays.find(type_index) != component_arrays.end()) {
+            auto components =
+                static_cast<SparseSet<T> *>(component_arrays[type_index].get());
+            return components->has(id);
+        }
+        return false;
+    }
+
+    template <typename T> T &get_component(EntityID id) {
+        std::type_index type_index = typeid(T);
+
+        assert(component_arrays.find(type_index) != component_arrays.end() &&
+               "[ERROR] No such component is registered.");
+
+        auto components =
+            static_cast<SparseSet<T> *>(component_arrays[type_index].get());
+
+        // Get handles component not existing
+        return components->get(id);
+    }
+
+    // NOTE: Best practice is to provide types in order of smallest to largest
+    // as this will mean the smallest loop
+    template <typename T, typename... OtherTypes> std::vector<EntityID> view() {
+        std::vector<EntityID> entities;
+
+        auto primary_set = get_component_set<T>();
+        if (!primary_set) {
+            return entities;
+        }
+
+        auto &dense = primary_set->get_dense_array();
+        for (int i = 0; i < dense.size(); i++) {
+            EntityID id = primary_set->get_entity(i);
+            if ((has_component<OtherTypes>(id) && ...)) {
+                entities.push_back(id);
+            }
+        }
+        return entities;
     }
 };
 } // namespace core
